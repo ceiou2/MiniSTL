d69dd0a5725b10bb5a8d7f005bb566ed51fe3c7a
caifunanxin <caifunanxin@scutch.com>
2024-08-09 11:59:03 +0800

    have reverse_iterator-a

---
+++ class_unordered_set/testUnorderedSet.cpp
@@ -0,0 +1,97 @@
+//testUnorderedSet.cpp
+#include<gtest/gtest.h>
+#include"MiniUnorderedSet.h"
+#include<vector>
+
+//测试构造函数
+TEST(testConstructor,defaultConstructor){
+    //默认构造函数
+    unordered_set<int> us;
+    EXPECT_TRUE(us.empty());
+}
+
+TEST(testConstructor, inputItConstructor)
+{
+    vector<int> v{1, 2, 3};
+    unordered_set<int> us(v.begin(),v.end());
+    EXPECT_EQ(us.size(),3);
+}
+
+TEST(testConstructor, CopyConstructor)
+{
+    vector<int> v{1, 2, 3};
+    unordered_set<int> us(v.begin(), v.end());
+    unordered_set<int> us2(us);
+    EXPECT_EQ(us2.size(), 3);
+}
+
+TEST(testConstructor, moveConstructor)
+{
+    vector<int> v{1, 2, 3};
+    unordered_set<int> us(v.begin(), v.end());
+    unordered_set<int> us2(std::move(us));
+    EXPECT_EQ(us2.size(), 3);
+    EXPECT_EQ(us.size(), 0);
+}
+
+TEST(testConstructor, InitConstructor)
+{
+    unordered_set<int> us{1,2,3};
+    EXPECT_EQ(us.size(), 3);
+}
+
+//测试operator=重载
+TEST(testOper,oper){
+    unordered_set<int> us{1, 2, 3};
+    unordered_set<int> us2 = us;
+    EXPECT_EQ(us2.size(), us.size());
+}
+
+//=========迭代器模块==========
+TEST(testIterator,Iterator){
+    unordered_set<int> us{1, 2, 3, 4, 5};
+    unordered_set<int>::iterator it1 = us.begin();
+    //operator*
+    EXPECT_EQ(*it1, 1);
+    //operator++
+    EXPECT_EQ(*(++it1), 2);
+    //operator++(int)
+    EXPECT_EQ(*(it1++), 2);
+    EXPECT_EQ(*it1, 3);
+    //operator--
+    EXPECT_EQ(*(--it1), 2);
+    //operator--(int)
+    EXPECT_EQ(*(it1--), 2);
+    EXPECT_EQ(*it1, 1);
+    //iterator operator =
+    auto it2 = it1;
+    EXPECT_TRUE(it1 == it2);
+    unordered_set<int>::const_iterator cit(it1);
+    EXPECT_TRUE(cit == it1);
+}
+
+TEST(testIterator, constIterator)
+{
+    unordered_set<int> us{1, 2, 3, 4, 5};
+    unordered_set<int>::const_iterator it1 = us.begin();
+    // operator*
+    EXPECT_EQ(*it1, 1);
+    // operator++
+    EXPECT_EQ(*(++it1), 2);
+    // operator++(int)
+    EXPECT_EQ(*(it1++), 2);
+    EXPECT_EQ(*it1, 3);
+    // operator--
+    EXPECT_EQ(*(--it1), 2);
+    // operator--(int)
+    EXPECT_EQ(*(it1--), 2);
+    EXPECT_EQ(*it1, 1);
+    // iterator operator =
+    auto it2 = it1;
+    EXPECT_TRUE(it1 == it2);
+}
+
+int main(int argc,char** argv){
+    testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
