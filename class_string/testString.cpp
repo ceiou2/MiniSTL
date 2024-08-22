494650f4716943f8d1792998673e21f637f363a9
caifunanxin <caifunanxin@scutch.com>
2024-08-22 18:49:18 +0800

    MiniString

--- class_string/testString.cpp
+++ class_string/testString.cpp
@@ -1,17 +1,25 @@
 #include<gtest/gtest.h>
 #include"MiniString.h"
 
+TEST(testStringConstructor,constructor){
+    string s;
+    EXPECT_EQ(s.size(), 0);
+    string s1(3, 'a');
+    EXPECT_EQ(s.size(),3);
+    string s2{'a','b','c'};
+    EXPECT_EQ(s2.size(), 3);
+    string s3(s2);
+    EXPECT_EQ(s2.size(), 3);
+    string smove(std::move(s3));
+    EXPECT_EQ(smove.size(), 3);
+    EXPECT_EQ(s3.size(),0);
+    string ss(5);
+    EXPECT_EQ(ss.size(), 5);
+}
 
+TEST(testIterator,iterator){
 
-
-
-
-
-
-
-
-
-
+}
 
 TEST(testString,data){
 
